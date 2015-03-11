7f173bffbc52ae6c5f38fbe4248974da0d272180
zyzo <zyzo@github.com>
2015-03-11 21:31:27 +0100

    page course-list : done

--- client/templates/course-list.js
+++ client/templates/course-list.js
@@ -2,4 +2,17 @@ Template.courseList.helpers({
     skillList : function() {
         return SkillList.find();
     }
-})
+});
+Template.courseList.events({
+    'click .removeBtn' : function(event) {
+        var skillName = $(event.target).parent().text();
+        // TODO : replace with publish/subscribe mechanism
+        var skillId = SkillList.findOne({name : skillName})._id;
+        SkillList.remove(skillId);
+    },
+    'click #modal-addCourseBtn' : function(event) {
+        var skillName = $('#modal-skillNameInput').val();
+        SkillList.insert({name : skillName});
+        $('#myModal').modal('hide');
+    }
+});
