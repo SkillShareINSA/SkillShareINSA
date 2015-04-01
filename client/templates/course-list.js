74808f94c487f57e92db6dd3712725b68fb69f38
RemiPrevost <prevost-r@hotmail.com>
2015-04-02 00:15:43 +0200

    Redirect step1

--- client/templates/course-list.js
+++ client/templates/course-list.js
@@ -1,3 +1,72 @@
+if (Meteor.isServer) {
+    Meteor.methods({
+        isConnected : function(name_user) {
+            
+        }
+    });
+}
+
+Template.defaultPage.helpers({
+    'start' : function() {
+    
+        function extractUrlParams(){    
+            var t = location.search.substring(1).split('&');
+            var f = [];
+            for (var i=0; i<t.length; i++){
+                var x = t[ i ].split('=');
+                f[x[0]]=x[1];
+            }
+            return f;
+        }
+
+        if (extractUrlParams().length != 0)  {
+
+        }  
+    }
+});
+
+
+
+Template.defaultPage.events({
+    'click #connect' : function(event) {
+    //     console.log("CLICKED §§§§");
+
+    //     var xhr = new XMLHttpRequest();
+    //     var file = "www.google.com";
+            
+    //     // xhr.ontimeout = function () {
+    //     //     console.log("Timed out");
+    //     //    /*TODO : do something about it*/
+    //     // }
+
+
+    //     xhr.onreadystatechange = function () {
+
+    //         if (xhr.readyState == 4) {
+    //             if (xhr.status >= 200 && xhr.status < 304) {
+    //                 console.log("Received something and succeced");
+    //             }
+    //             else {
+    //                 console.log("Received something and failed");
+    //                 /*TODO : do something about it*/
+    //             }
+    //         }
+    //     }
+
+    //     xhr.open('GET', file, false);
+    //     //xhr.timeout = 10000;
+
+    //     try {
+    //         console.log("xhr : "+xhr);
+    //         xhr.send();
+    //         console.log("after");
+    //     } catch (e) {
+    //         /*TODO : do something about it*/
+    //     }
+    }
+});
+
+
 Template.courseList.helpers({
     skillList : function() {
         return SkillList.find();
