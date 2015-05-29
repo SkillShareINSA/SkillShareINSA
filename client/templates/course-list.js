bd1bcc4b77f8517974d31c893931df89981836a0
zyzo <zyzo@github.com>
2015-05-29 21:30:17 +0200

    remove unused template seekHelp

--- client/templates/course-list.js
+++ client/templates/course-list.js
@@ -53,32 +53,6 @@ Template.addCoursePopup.events({
     }
 });
 
-
-    Template.seekHelp.helpers({
-        'skillList' : function() {
-            return SkillList.find();
-        },
-
-        'getTeachersOf' : function(skillName) {
-            if (Meteor.isClient)
-                console.log("Je suis client");
-            else
-                console.log("Je suis serveur");
-            console.log("SKILLNAME: "+skillName);
-            var skill = SkillList.findOne({name : skillName});
-            console.log(skill.teachers);
-            return skill.teachers;
-        }
-    });
-
-Template.postConnect.onCreated = function() {
-    Accounts.callLoginMethod({
-        methodArguments: [{username: Router.current().params.query.user}]
-    });
-    //Meteor.call('login_requested',Router.current().params.query.user);
-    Router.go('courses');
-};
-
 Template.logout.logout = function() {
     Meteor.logout();
     Meteor.call('logout_requested',Router.current().params.query.user);
--- client/templates/help-research.js
+++
@@ -1,5 +0,0 @@
-Template.courseList.helpers({
-    skillList : function() {
-        return SkillList.find();
-    }
-});
