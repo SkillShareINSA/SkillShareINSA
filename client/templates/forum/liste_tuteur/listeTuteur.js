dc13b340a17919b4b98e4f006e0a12d7d8d08976
Felix Wang Chao <felixwangchao@github.com>
2015-06-02 21:16:57 +0800

    ajouter boutton

--- client/templates/forum/liste_tuteur/listeTuteur.js
+++ client/templates/forum/liste_tuteur/listeTuteur.js
@@ -5,7 +5,13 @@ Template.listeTuteur.helpers({
     liste:function(){
         console.log(this._id);
         return SkillList.find({id:this.id}).teachers;
+    },
+    matiereName: function(){
+        var skill_name = SkillList.findOne({_id :this._id}).name;
+        console.log(skill_name);
+        return skill_name;
     }
+
 })
 
 Template.listeTuteur.events({
--- client/templates/forum/posts/matierePost.js
+++ client/templates/forum/posts/matierePost.js
@@ -4,7 +4,12 @@
 Template.matierePost.helpers({
     matierePage: function() {
         console.log(this._id);
-        return Session.get('matierePage'); }
+        return Session.get('matierePage'); },
+    matiereName: function(){
+        var skill_name = SkillList.findOne({_id :this._id}).name;
+        console.log(skill_name);
+        return skill_name;
+    }
 })
 
 Template.matierePost.events({
