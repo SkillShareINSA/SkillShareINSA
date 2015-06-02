7ce640140c9e1e2178f5fcca4c63acb5747bb69e
RemiPrevost <prevost-r@hotmail.com>
2015-06-02 16:57:33 +0200

    L'aigle était mieux...

--- client/templates/utils/sidebar.js
+++ client/templates/utils/sidebar.js
@@ -1,13 +1,12 @@
  Template.sidebar.helpers({
             nbUnreadMessages : function() {
-            if(Meteor.userId() != null){
+            if(Meteor.userId() != null && typeof Meteor.userId() != 'undefined'){
                 console.log("On est bien rentré dans le helper");
                 var nb_messages = Meteor.user().nb_unread_messages;
                 if(nb_messages > 99) return "99+";
                 	else return nb_messages;
             }
-        
             else
-            return "";
+            	return "";
             }
         });
