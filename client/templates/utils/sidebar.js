5683331420146ccdc31f1fc8655de19346d2b5c6
RemiPrevost <prevost-r@hotmail.com>
2015-06-02 15:25:58 +0200

    Valeur max badge messages non lus

--- client/templates/utils/sidebar.js
+++ client/templates/utils/sidebar.js
@@ -2,7 +2,9 @@
             nbUnreadMessages : function() {
             if(Meteor.userId() != null){
                 console.log("On est bien rentré dans le helper");
-                return Meteor.user().nb_unread_messages;
+                var nb_messages = Meteor.user().nb_unread_messages;
+                if(nb_messages > 99) return "99+";
+                	else return nb_messages;
             }
         
             else
