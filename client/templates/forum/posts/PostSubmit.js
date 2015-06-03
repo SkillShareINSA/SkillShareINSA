28876bfbb5490c98c4d4a13d433518634a130210
Felix Wang Chao <felixwangchao@github.com>
2015-06-03 23:04:20 +0800

    "petites modifications-3"

--- client/templates/forum/posts/PostSubmit.js
+++ client/templates/forum/posts/PostSubmit.js
@@ -10,6 +10,15 @@ Template.PostSubmit.events({
             matiereName:matiereName_tmp
         };
 
+        console.log("j'ai un probleme");
+
+        var errors = {};
+        if (! post.title) {
+            console.log("entrer");
+            errors.body = "Le sujet est vide";
+            return Session.set('postSubmitErrors', errors);
+        }
+
         Meteor.call('postInsert', post, function(error, result) {
             // 向用户显示错误信息并终止
             if (error)
@@ -18,6 +27,10 @@ Template.PostSubmit.events({
             // 显示结果，跳转页面
             if (result.postExists)
                 alert('This link has already been posted（该链接已经存在）');
+
+            if (result.sujetVide)
+                 return alert('Le sujet est vide');
+
 
             console.log(template.data._id);
 
